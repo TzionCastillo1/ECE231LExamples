9dd473ca909d4b5fa15727323d70a9031356fffb
Tzion Castillo <tzionmcastillo@gmail.com>
2022-10-27 12:36:59 -0600

    Notes and Examples from Oct 27th, continuation of templates

--- Templates/linklist/singlelist.hpp
+++ Templates/linklist/singlelist.hpp
@@ -1,7 +1,8 @@
 #include<cstddef>
-
-/*We are implementing our code within the header file. This is only acceptable for templates
-* We would get an error if we split it into header and implementation files
+#include<cassert>
+/*
+ * We are implementing our code within the header file. This is only acceptable for templates
+ * We would get an error if we split it into header and implementation files
 */
 
 
@@ -15,7 +16,8 @@ class SingleList {
 
 		/*Below is called type aliasing*/
 		using Node = struct _node;
-		/*Alternative is typedef Node struct _node;*/
+		/*Alternative is 
+		 * typedef Node struct _node;*/
 
 		Node* _first;
 		Node* _last;
@@ -52,10 +54,75 @@ class SingleList {
 			return _size;
 		}
 
+		void push_back(T value){
+			SingleList* item = new Node;
+			item->data = value;
+			item->next = nullptr;
+			if(empty()){
+				_first = item;
+				}
+			else{
+				_last->next = item;
+				}
+			_last = item;
+			_size++;
+		}
+
+		void push_front(T value){
+			SingleList* item = new Node;
+			item->data = value;
+			if(empty()){
+				_last = item;
+				}
+			else{
+				item->next = _first;
+				}
+			_first = item;
+			_size++;
+
+		}
+
+		void pop_front(){
+			/*if(empty()){
+				return;	
+			}
+			 */
+			assert(!empty());
+			SingleList* node_to_delete = _first;
+			if(_size = 1){
+				_first = nullptr;
+				_last = nullptr;
+				}
+			else{
+				_first = _first->next;
+				}
+			delete node_to_delete;
+			size--;
+
+		}
+
+		void pop_back(){
+			assert(!empty());
+			SingleList* node_to_delete = _last;
+			if(_size = 1){
+				_first = nullptr;
+				_last = nullptr;
+			}
+			else{
+				//define new _last
+				SingleList* new_last = _first;
+				while(new_last->next != _last){
+					new_last = new_last->next;
+				}
+				_last = new_last;
+				_last->next = nullptr;
+			}
+			delete node_to_delete;
+			size--;
+		}
+
 		bool empty() const {
 			return (_first == nullptr) && (_last == nullptr)\
 							&& (_size == 0);
 		}
-
-
 };
